9e75c43d0da9de2df458b69133b65b9654a9dc3a
Taran Zorn <diamondfellow14@gmail.com>
2020-10-16 10:35:28 -0700

    stuff

---
+++ LlamaFarm2playe/Assets/Scripts/CornGrow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CornGrow : MonoBehaviour
+{
+    int waterLevel, growlevel;
+    
+
+    float growtimer;
+    float watertimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        waterLevel = 100;
+        growlevel = 100;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        growtimer += Time.deltaTime;
+        watertimer += Time.deltaTime;
+    }
+    public void CanvasUpdate()
+    {
+
+    }
+}
--- LlamaFarm2playe/Assets/Scripts/GameController.cs
+++ LlamaFarm2playe/Assets/Scripts/GameController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public int waterMaxTimer;
+    public int growMaxTimer;
     // Start is called before the first frame update
     void Start()
     {
