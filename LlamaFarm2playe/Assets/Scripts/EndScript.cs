490a946dde11b5a0f566b695b390620938a8c037
Conner Anderson <connerwithahat@gmail.com>
2020-10-17 17:16:23 -0700

    Finished end scene

---
+++ LlamaFarm2playe/Assets/Scripts/EndScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScript : MonoBehaviour
+{
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = "You harvested " + PlayerPrefs.GetInt("Corn") + " corn!";
+    }
+
+    public void BrowserOpen()
+    {
+        System.Diagnostics.Process.Start("https://www.feedingamerica.org/find-your-local-foodbank");
+    }
+}
